faba39dd2a3e9c025605e9ac434b38d31014b322
Hector Cosme <me@hcosme.com>
2017-07-24 21:34:39 -0400

    07/24/2017/21:34

--- swift fun/ViewController.swift
+++ swift fun/ViewController.swift
@@ -11,16 +11,34 @@ import UIKit
 class ViewController: UIViewController {
     var buttonCount = 0
     var countDown = 10
+    var sum = 0.0
     @IBOutlet weak var myLabel: UILabel!
-    @IBOutlet var myCountDown: UIView!
-    @IBAction func changeText(_ sender: Any) {
-        buttonCount = buttonCount + 1
-        countDown = countDown - 1
-        view.backgroundColor = UIColor.darkGray
-        myLabel.text = "yeah"
-        myCountDown.text = countDown.
+    @IBOutlet weak var myCountDown: UILabel!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var operationSwitch: UISwitch!
     
+    @IBAction func changeText(_ sender: Any) {
+        
+        let operation = operationSwitch.isOn
+        if operation {
+            sum = Double(firstNumber.text!)! + Double(secondNumber.text!)!
+        print(firstNumber.text!)
+        print(secondNumber.text!)
+        }else{
+            sum = Double(firstNumber.text!)! - Double(secondNumber.text!)!
+            print(firstNumber.text!)
+            print(secondNumber.text!)
+        }
+        buttonCount += 1
+        countDown -= 1
+        view.backgroundColor = UIColor.darkGray
+        myLabel.text = "The sum is \(sum)"
+    
+        myCountDown.text = String(countDown)
+        print(countDown)
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
